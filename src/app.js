9930ad0d04cf70664cc3112ba5ccceac122a2b7a
james@jamespamplin.com <james@jamespamplin.com>
2016-08-25 00:19:46 +0100

    Prototype of basic circle interaction in UI. Fixed flow config

--- src/app.js
+++ src/app.js
@@ -1,7 +1,27 @@
 //@flow
+/*global document*/
 
-const test: string = 'hi';
+const SVG_NS = 'http://www.w3.org/2000/svg';
+
+function initialise( elem: HTMLElement ) {
+  const handler: EventHandler = worldClicked.bind( null, elem );
+
+  elem.addEventListener( 'click', handler );
+}
+
+function worldClicked( elem: HTMLElement, event: MouseEvent ) {
+  const circleElement = makeCircle( event.clientX, event.clientY, 10 );
+  elem.appendChild( circleElement );
+}
+
+function makeCircle( cx: number, cy: number, radius: number ): Node {
+  const circleElement = document.createElementNS( SVG_NS, 'circle' );
+  circleElement.setAttribute( 'cx', cx.toString() );
+  circleElement.setAttribute( 'cy', cy.toString() );
+  circleElement.setAttribute( 'r', radius.toString() );
+  return circleElement;
+}
 
 export default {
-  hello: 'world ' + test
+  initialise
 };
