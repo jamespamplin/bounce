66011e102a5e0e6edc877a9d6a1cd8d547d7f4e9
james@jamespamplin.com <james@jamespamplin.com>
2016-08-24 22:34:04 +0100

    Initial rollup+babel build config

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+import babel from 'rollup-plugin-babel';
+
+export default {
+  entry: 'src/app.js',
+  dest: 'dist/app.js',
+  format: 'umd',
+  moduleName: 'app',
+  sourceMap: true,
+  plugins: [ babel() ]
+};
